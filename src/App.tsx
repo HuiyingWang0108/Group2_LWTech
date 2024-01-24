7b83fe8a467a3c38bbf1b6368344ad54a1737066
ZoeWings <zoe.wings007@gmail.com>
2024-01-23 20:36:50 -0800

    Change text and color of home

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Group 2
         </a>
       </header>
     </div>
